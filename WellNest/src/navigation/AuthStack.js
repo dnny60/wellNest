c2027cfeadd18fb2a62b922582e9b15e9f149fab
winnie2qui0 <114901064+winnie2qui0@users.noreply.github.com>
2024-06-01 14:55:17 +0800

    surveyscreen
    心理測驗前端

--- WellNest/src/navigation/AuthStack.js
+++ WellNest/src/navigation/AuthStack.js
@@ -5,9 +5,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import OnboardingScreen from '../screens/OnboardingScreen';
 import LoginScreen from '../screens/LoginScreen';
 import RegisterScreen from '../screens/RegisterScreen';
-import ProfileScreen from '../screens/ProfileScreen';
+import SurveyScreen from '../screens/SurveyScreen';
+import Question1 from '../screens/Question1'
 import {AuthContext} from '../components/AuthContext';
-import HomeScreen from '../screens/HomeScreen';
 import AppStack from './AppStack';
 
 const Stack = createNativeStackNavigator();
@@ -15,23 +15,37 @@ const Stack = createNativeStackNavigator();
 const AuthStack = () => {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(null);
 
+  // useEffect(() => {
+  //   const checkLoginStatus = async () => {
+  //     try {
+  //       const token = await AsyncStorage.getItem('userToken');
+  //       console.log('Token:', token);
+  //       //true false記得改回來
+  //       if (token) {
+  //         setIsUserLoggedIn(true);
+  //       } else {
+  //         setIsUserLoggedIn(false);
+  //       }
+  //     } catch (e) {
+  //       console.error(e);
+  //     }
+  //   };
+
+  //   checkLoginStatus();
+  // }, []);
+
   useEffect(() => {
     const checkLoginStatus = async () => {
-      try {
-        const token = await AsyncStorage.getItem('userToken');
-        console.log('Token:', token);
-        if (token) {
-          setIsUserLoggedIn(true);
-        } else {
-          setIsUserLoggedIn(false);
-        }
-      } catch (e) {
-        console.error(e);
-      }
+      // 这里我们不从 AsyncStorage 获取token，
+      // 而是直接设置一个前端测试的登录状态，true为已登录，false为未登录
+      const testLoggedInStatus = false; // 改为true来模拟已登录状态，false表示未登录
+      setIsUserLoggedIn(testLoggedInStatus);
     };
 
     checkLoginStatus();
   }, []);
+
+
 
   if (isUserLoggedIn === null) {
     return null; // 或者加载中的指示器
@@ -43,7 +57,7 @@ const AuthStack = () => {
           // 如果用户已登录，直接显示 ProfileScreen
           <>
             {/* <Stack.Screen name="Profile" component={ProfileScreen} /> */}
-            <Stack.Screen name="AppStack" component={<AppStack />  } />
+            <Stack.Screen name="AppStack" component={AppStack} />
           </>
         ) : (
           // 如果用户未登录，显示完整的登录流程
@@ -51,6 +65,8 @@ const AuthStack = () => {
             <Stack.Screen name="Onboarding" component={OnboardingScreen} />
             <Stack.Screen name="Login" component={LoginScreen} />
             <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Survey" component={SurveyScreen} />
+            <Stack.Screen name="Q1" component={Question1} />
           </>
         )}
       </Stack.Navigator>
